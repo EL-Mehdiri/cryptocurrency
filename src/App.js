41cdd15ee57a4eed5db251550efd4f96870fec7e
MehdiRidmani <mahdiridmani22@gmail.com>
2023-06-27 22:58:18 +0100

    new components

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 
+import Navbar from './components';
+
 const App = () => {
     return (
         <div className='app'>
             <div className='navbar'>
-
+                <Navbar />
             </div>
             <div className='main'>
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,19 +3,20 @@ import { Button, Menu, Typography, Avatar } from 'antd';
 import { Link } from 'react-router-dom/dist';
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
 
-
+import icon from '../images/cryptocurrency.png'
 
 const Navbar = () => {
     return (
         <div className='nav-container'>
             <div className='logo-container'>
-                <Avatar />
+                {/* <Avatar src={icon} />
                 <Typography.title level={2} className='logo' >
                     <Link to='/'>Cryptoverse</Link>
-                </Typography.title>
-                <button className='menu-control-container'>
+                </Typography.title> */}
+                {/* <button className='menu-control-container'>
 
-                </button>
+                </button> */}
+                <h1>hi amal</h1>
             </div>
         </div>
     )
